d237d67741b0921430877261018c953d91eded49
vieiramanda11 <amanda_vieira@outlook.com>
2019-09-24 18:41:40 -0300

    writing tests for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
-    validates :comment, presence:true, length: {in: 1..250}
+    validates :comment, presence:true, length: {in: 5..250}
 end
