0b302193dfa7ea7eacace51dc034e145342aed0b
vieiramanda11 <amanda_vieira@outlook.com>
2019-09-24 18:15:09 -0300

    adding user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   has_many :posts
   validates :name, presence: true, length: { in: 5..20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
