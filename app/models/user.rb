d15a7feb5bda4e9ff0c8572a0e8fd4df7ee4cbff
vieiramanda11 <amanda_vieira@outlook.com>
2019-09-24 17:10:23 -0300

    creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { in: 5..20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { in: 6..20 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false } 
 end
